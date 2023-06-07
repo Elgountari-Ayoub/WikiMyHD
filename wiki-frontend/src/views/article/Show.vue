8b7ef56957083db26b660c8fb08b60de79e28f75
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-06-07 14:10:22 +0100

    the menu is ready

--- wiki-frontend/src/views/article/Show.vue
+++ wiki-frontend/src/views/article/Show.vue
@@ -11,12 +11,8 @@
 
 <script setup>
 import DashboardLayout from '../../components/layouts/DashboardLayout.vue';
-import { ref, watchEffect, onMounted, watch } from 'vue';
-import axios from 'axios';
-import { RouterView, useRouter } from 'vue-router';
+import { RouterView } from 'vue-router';
 import Tiptap from '../../components/editor/Tiptap.vue';
-
-axios.defaults.withCredentials = true;
 
 
 </script>
