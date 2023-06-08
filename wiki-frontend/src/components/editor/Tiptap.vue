55d215d39c2b82e809fba69d8f903fd2e0e00040
Elgountari-Ayoub <elgountariayoub22@gmail.com>
2023-06-08 13:57:02 +0100

    add image and link extentions

--- wiki-frontend/src/components/editor/Tiptap.vue
+++ wiki-frontend/src/components/editor/Tiptap.vue
@@ -1,170 +1,233 @@
 <template>
-  <div class="min-h-[80vh]">
-    <div v-if="editor" class="cmds bg-black p-2 rounded">
-      <button title="bold" @click="editor.chain().focus().toggleBold().run()"
-        :disabled="!editor.can().chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
-        <i class="ri-bold"></i>
-      </button>
-      <button title="italic" @click="editor.chain().focus().toggleItalic().run()"
-        :disabled="!editor.can().chain().focus().toggleItalic().run()"
-        :class="{ 'is-active': editor.isActive('italic') }">
-        <i class="ri-italic"></i>
-      </button>
-      <button title="strike" @click="editor.chain().focus().toggleStrike().run()"
-        :disabled="!editor.can().chain().focus().toggleStrike().run()"
-        :class="{ 'is-active': editor.isActive('strike') }">
-        <i class="ri-strikethrough"></i>
-      </button>
+  <!-- <div v-if="editor" class="cmds bg-black m-auto p-2 rounded w-9/12 ml-10"> -->
+  <div v-if="editor" class="cmds bg-black m-auto p-2 rounded ">
+    <button title="image" @click="addImage">
+      <i class="ri-image-add-line"></i>
+    </button>
 
-      <button @click="editor.chain().focus().toggleHighlight().run()"
-        :class="{ 'is-active': editor.isActive('highlight') }">
-        <i class="ri-mark-pen-line"></i>
+    <div class="divider"></div>
 
-      </button>
+    <button title="link" @click="toggleLink" :class="{ 'is-active': editor.isActive('link') }">
+      <i class="ri-link"></i>
+    </button>
+
+    <div class="divider"></div>
+
+    <button title="bold" @click="editor.chain().focus().toggleBold().run()"
+      :disabled="!editor.can().chain().focus().toggleBold().run()" :class="{ 'is-active': editor.isActive('bold') }">
+      <i class="ri-bold"></i>
+    </button>
+    <button title="italic" @click="editor.chain().focus().toggleItalic().run()"
+      :disabled="!editor.can().chain().focus().toggleItalic().run()" :class="{ 'is-active': editor.isActive('italic') }">
+      <i class="ri-italic"></i>
+    </button>
+    <button title="strike" @click="editor.chain().focus().toggleStrike().run()"
+      :disabled="!editor.can().chain().focus().toggleStrike().run()" :class="{ 'is-active': editor.isActive('strike') }">
+      <i class="ri-strikethrough"></i>
+    </button>
+
+    <button @click="editor.chain().focus().toggleHighlight().run()"
+      :class="{ 'is-active': editor.isActive('highlight') }">
+      <i class="ri-mark-pen-line"></i>
+
+    </button>
 
 
-      <button title="code" @click="editor.chain().focus().toggleCode().run()"
-        :disabled="!editor.can().chain().focus().toggleCode().run()" :class="{ 'is-active': editor.isActive('code') }">
-        <i class="ri-code-fill"></i>
-      </button>
+    <button title="code" @click="editor.chain().focus().toggleCode().run()"
+      :disabled="!editor.can().chain().focus().toggleCode().run()" :class="{ 'is-active': editor.isActive('code') }">
+      <i class="ri-code-fill"></i>
+    </button>
 
-      <button title="clear marks" @click="editor.chain().focus().unsetAllMarks().run()">
-        <i class="ri-strikethrough-2"></i>
-      </button>
+    <button title="clear marks" @click="editor.chain().focus().unsetAllMarks().run()">
+      <i class="ri-strikethrough-2"></i>
+    </button>
 
-      <div class="divider"></div>
+    <div class="divider"></div>
 
-      <button title="clear nodes" @click="editor.chain().focus().clearNodes().run()">
-        <i class="ri-format-clear"></i>
-      </button>
-      <button title="paragraph" @click="editor.chain().focus().setParagraph().run()"
-        :class="{ 'is-active': editor.isActive('paragraph') }">
-        <i class="ri-paragraph"></i>
-      </button>
-      <button title="h1" @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
-        <i class="ri-h-1"></i>
-      </button>
-      <button title="h2" @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
-        <i class="ri-h-2"></i>
-      </button>
-      <button title="h3" @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
-        <i class="ri-h-3"></i>
-      </button>
-      <button title="h4" @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
-        <i class="ri-h-4"></i>
-      </button>
-      <button title="h5" @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
-        <i class="ri-h-5"></i>
-      </button>
-      <button title="h6" @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
-        :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }">
-        <i class="ri-h-6"></i>
-      </button>
-      <button title="bullet list" @click="editor.chain().focus().toggleBulletList().run()"
-        :class="{ 'is-active': editor.isActive('bulletList') }">
-        <i class="ri-list-unordered"></i>
-      </button>
-      <button title="ordered list" @click="editor.chain().focus().toggleOrderedList().run()"
-        :class="{ 'is-active': editor.isActive('orderedList') }">
-        <i class="ri-list-ordered"></i>
-      </button>
-      <button title="code block" @click="editor.chain().focus().toggleCodeBlock().run()"
-        :class="{ 'is-active': editor.isActive('codeBlock') }">
-        <i class="ri-code-box-line"></i>
-      </button>
-      <button title="blockquote" @click="editor.chain().focus().toggleBlockquote().run()"
-        :class="{ 'is-active': editor.isActive('blockquote') }">
-        <i class="ri-double-quotes-l"></i>
-      </button>
-      <button title="horizontal rule" @click="editor.chain().focus().setHorizontalRule().run()">
-        <i class="ri-separator"></i>
-      </button>
-      <button title="hard break" @click="editor.chain().focus().setHardBreak().run()">
-        <i class="ri-text-wrap"></i>
-      </button>
-      <button title="undo" @click="editor.chain().focus().undo().run()"
-        :disabled="!editor.can().chain().focus().undo().run()">
-        <i class="ri-arrow-go-back-line"></i>
-      </button>
-      <button title="redo" @click="editor.chain().focus().redo().run()"
-        :disabled="!editor.can().chain().focus().redo().run()">
-        <i class="ri-arrow-go-forward-line"></i>
-      </button>
-    </div>
+    <button title="clear nodes" @click="editor.chain().focus().clearNodes().run()">
+      <i class="ri-format-clear"></i>
+    </button>
+    <button title="paragraph" @click="editor.chain().focus().setParagraph().run()"
+      :class="{ 'is-active': editor.isActive('paragraph') }">
+      <i class="ri-paragraph"></i>
+    </button>
+    <button title="h1" @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }">
+      <i class="ri-h-1"></i>
+    </button>
+    <button title="h2" @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }">
+      <i class="ri-h-2"></i>
+    </button>
+    <button title="h3" @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }">
+      <i class="ri-h-3"></i>
+    </button>
+    <button title="h4" @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }">
+      <i class="ri-h-4"></i>
+    </button>
+    <button title="h5" @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }">
+      <i class="ri-h-5"></i>
+    </button>
+    <button title="h6" @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
+      :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }">
+      <i class="ri-h-6"></i>
+    </button>
 
-    <editor-content :editor="editor" class="border-solid border-2 border-black rounded" />
+    <div class="divider"></div>
+
+    <button title="bullet list" @click="editor.chain().focus().toggleBulletList().run()"
+      :class="{ 'is-active': editor.isActive('bulletList') }">
+      <i class="ri-list-unordered"></i>
+    </button>
+    <button title="ordered list" @click="editor.chain().focus().toggleOrderedList().run()"
+      :class="{ 'is-active': editor.isActive('orderedList') }">
+      <i class="ri-list-ordered"></i>
+    </button>
+    <button title="code block" @click="editor.chain().focus().toggleCodeBlock().run()"
+      :class="{ 'is-active': editor.isActive('codeBlock') }">
+      <i class="ri-code-box-line"></i>
+    </button>
+    <button title="blockquote" @click="editor.chain().focus().toggleBlockquote().run()"
+      :class="{ 'is-active': editor.isActive('blockquote') }">
+      <i class="ri-double-quotes-l"></i>
+    </button>
+    <button title="horizontal rule" @click="editor.chain().focus().setHorizontalRule().run()">
+      <i class="ri-separator"></i>
+    </button>
+
+    <div class="divider"></div>
+
+    <button title="hard break" @click="editor.chain().focus().setHardBreak().run()">
+      <i class="ri-text-wrap"></i>
+    </button>
+    <button title="undo" @click="editor.chain().focus().undo().run()"
+      :disabled="!editor.can().chain().focus().undo().run()">
+      <i class="ri-arrow-go-back-line"></i>
+    </button>
+    <button title="redo" @click="editor.chain().focus().redo().run()"
+      :disabled="!editor.can().chain().focus().redo().run()">
+      <i class="ri-arrow-go-forward-line"></i>
+    </button>
   </div>
+
+  <editor-content :editor="editor" class="h-[80vh] overflow-auto my-editor-content" />
 </template>
 
-<script>
+<script setup>
+import Link from '@tiptap/extension-link'
+import Dropcursor from '@tiptap/extension-dropcursor'
+import Image from '@tiptap/extension-image'
+import Highlight from '@tiptap/extension-highlight'
 import StarterKit from '@tiptap/starter-kit'
 import { Editor, EditorContent } from '@tiptap/vue-3'
-import Highlight from '@tiptap/extension-highlight'
+import { onBeforeUnmount } from 'vue';
 
-export default {
-  components: {
-    EditorContent,
-  },
-
-  data() {
-    return {
-      editor: null,
-    }
-  },
-
-  mounted() {
-    this.editor = new Editor({
-      extensions: [
-        StarterKit,
-        Highlight
-      ],
-      content: `
-        <h2>
-          Hi there,
-        </h2>
-        <p>
-          this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
-        </p>
-        <ul>
-          <li>
-            That’s a bullet list with one …
-          </li>
-          <li>
-            … or two list items.
-          </li>
-        </ul>
-        <p>
-          Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
-        </p>
-        <pre><code class="language-css">body {
-  display: none;
+const editor = new Editor({
+  extensions: [
+    StarterKit,
+    Highlight,
+    Image,
+    Dropcursor,
+    Link
+  ],
+  content: `
+    <h2>
+      Hi there,
+    </h2>
+    <p>
+      this is a <em>basic</em> example of <strong>tiptap</strong>. Sure, there are all kind of basic text styles you’d probably expect from a text editor. But wait until you see the lists:
+    </p>
+    <ul>
+      <li>
+        That’s a bullet list with one …
+      </li>
+      <li>
+        … or two list items.
+      </li>
+    </ul>
+    <p>
+      Isn’t that great? And all of that is editable. But wait, there’s more. Let’s try a code block:
+    </p>
+    <pre><code class="language-css">body {
+display: none;
 }</code></pre>
-        <p>
-          I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
-        </p>
-        <blockquote>
-          Wow, that’s amazing. Good work, boy! 👏
-          <br />
-          — Mom
-        </blockquote>
-      `,
-    })
-  },
+    <p>
+      I know, I know, this is impressive. It’s only the tip of the iceberg though. Give it a try and click a little bit around. Don’t forget to check the other examples too.
+    </p>
+    <blockquote>
+      Wow, that’s amazing. Good work, boy! 👏
+      <br />
+      — Mom
+    </blockquote>
+    <img src="https://source.unsplash.com/K9QHL52rE2k/800x400" />
+      
+  `,
+})
 
-  beforeUnmount() {
-    this.editor.destroy()
-  },
+const toggleLink = () => {
+  if (editor.isActive('link')) {
+    editor.chain().focus().unsetLink().run();
+  } else {
+    const previousUrl = editor.getAttributes('link').href;
+    const url = window.prompt('URL', previousUrl);
+
+    // cancelled
+    if (url === null) {
+      return;
+    }
+
+    // empty
+    if (url === '') {
+      editor
+        .chain()
+        .focus()
+        .extendMarkRange('link')
+        .unsetLink()
+        .run();
+
+      return;
+    }
+
+    // update link
+    editor
+      .chain()
+      .focus()
+      .extendMarkRange('link')
+      .setLink({ href: url })
+      .run();
+  }
 }
+const addImage = () => {
+  const url = window.prompt('URL')
+
+  if (url) {
+    editor.chain().focus().setImage({ src: url }).run()
+  }
+}
+onBeforeUnmount(() => {
+  editor.destroy()
+})
+
 </script>
 
-<style lang="scss">
+<style lang="scss" >
+div.cmds {
+  position: fixed;
+  z-index: 999;
+  // box-shadow:  0px -20px 30px black;
+  // width: 100rem;
+  top: 3.6rem;
+  width: calc(100% - 21%);
+
+  // left: 19%;
+}
+
 div.cmds * {
   all: revert;
+  // position: fixed;
 }
 
 div.cmds button {
@@ -196,10 +259,30 @@ div.cmds .divider {
   display: inline-block;
 }
 
+
+
 /* Basic editor styles */
+::-webkit-scrollbar {
+  width: .2rem;
+
+}
+
+::-webkit-scrollbar-track {
+  background: white; 
+}
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: black;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
+}
+
 .ProseMirror {
-  outline: 0rem;
-  margin: 2rem;
+  // outline: 1rem;
+  padding: 5rem;
+
 
   >*+* {
     margin-top: 0.75em;
@@ -208,6 +291,8 @@ div.cmds .divider {
   * {
     all: revert;
   }
+
+
 
   ul,
   ol {
@@ -246,7 +331,25 @@ div.cmds .divider {
   img {
     max-width: 100%;
     height: auto;
+
+    &.ProseMirror-selectednode {
+      outline: 3px solid #68CEF8;
+    }
   }
+
+  a {
+    color: #68CEF8;
+  }
+
+  code {
+    font-size: 0.9rem;
+    padding: 0.25em;
+    border-radius: 0.25em;
+    background-color: rgba(#616161, 0.1);
+    color: #616161;
+    box-decoration-break: clone;
+  }
+
 
   blockquote {
     padding-left: 1rem;
@@ -258,4 +361,5 @@ div.cmds .divider {
     border-top: 2px solid rgba(#0D0D0D, 0.1);
     margin: 2rem 0;
   }
-}</style>
+}
+</style>
--- wiki-frontend/src/views/article/Show.vue
+++ wiki-frontend/src/views/article/Show.vue
@@ -3,7 +3,73 @@
     <RouterView />
     <div>
         <DashboardLayout>
-            <tiptap  />
+            <!-- <div class="fixed bg-white h-16 top-10 z-40 w-[75rem]"></div> -->
+            <div class="min-h-[80vh] flex flex-col ">
+                <tiptap />
+                <button class="bg-green-500 text-white px-4 py-2 rounded-full m-auto w-fit mt-2">Submit Article</button>
+            </div>
+        <!-- <div class="menu">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam mollitia iste eaque sed quo. Ea cum dolore
+                nisi omnis quia, reprehenderit id delectus reiciendis assumenda laborum aspernatur non laboriosam at.
+            </div>
+            <div class="content">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quae odit aspernatur ab consectetur
+                accusamus
+                nisi modi, quibusdam, libero voluptatum tempore eveniet iure. Dolores est cumque expedita incidunt
+                sapiente ad debitis!
+                </div> -->
         </DashboardLayout>
     </div>
 </template>
@@ -13,10 +79,18 @@
 import DashboardLayout from '../../components/layouts/DashboardLayout.vue';
 import { RouterView } from 'vue-router';
 import Tiptap from '../../components/editor/Tiptap.vue';
-
-
 </script>
 
 <style>
+.menu {
+    background-color: black;
+    color: white;
+}
 
+.content {
+    background-color: gray;
+    height: 100px;
+    overflow: scroll;
+
+}
 </style>
